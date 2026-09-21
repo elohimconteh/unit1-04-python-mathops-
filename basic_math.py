5ced5c287489d8d1ae07a37bafee74de6da393c4
Elohim Conteh <elohim.conteh@stu.ocsb.ca>
2026-09-21 23:46:36 +0000

    First commit of basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: elohim
+
+# Date: sep 12, 2026
+
+
+# This program displays a a series of math equations.
+def main():
+    print("2 + 3 = {}".format(2 + 3))
+    print("5 - 4 = {}".format(6 - 3))
+    print(" 7 * 3 = {}".format(220 * 3))
+    print("250 / 3 = {}".format(250 / 3))
+    print("2 ** 3 = {}".format(2**3))
+    print("16 ** 0.5 = {}".format(16**0.5))  # square root
+    print("1 + 3 * 4 = {}".format(1 + 3 * 4))
+    print("10 / 5 - 2 = {}".format(10 / 5 - 2))
+
+
+if __name__ == "__main__":
+    main()
